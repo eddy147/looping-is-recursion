a5ca5a706d44ee061a474cb0d5e0fa9906d58ee7
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-10-16 16:16:44 +0200

    master: avg is next

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,16 +1,44 @@
 (ns looping-is-recursion)
 
-(defn power [base exp]
-  ":(")
+(defn recur-factorial [number]
+  (let [helper (fn [acc n]
+                 (if (zero? n)
+                   acc
+                   (recur (* acc n) (dec n))))]
+    (helper 1 number)))
+
+(defn power-helper [y, x, n]
+  (cond
+    (< n 0) (recur y (/ 1 x) (- n))
+    (zero? n) y
+    (= 1 n) (* x y)
+    (even? n)
+    (recur y (* x x) (/ n 2))
+    :else (recur (* x y) (* x x) (/ (dec n) 2))))
+
+(defn power [base, exp]
+  (power-helper 1 base exp))
 
 (defn last-element [a-seq]
-  ":(")
+  (cond
+    (empty? a-seq) nil
+    (= 1 (count a-seq)) (first a-seq)
+    :else (recur (rest a-seq))))
 
-(defn seq= [seq1 seq2]
-  ":(")
+(defn seq= [a-seq b-seq]
+  (cond
+    (and (empty? a-seq) (empty? b-seq)) true
+    (or (empty? a-seq) (empty? b-seq)) false
+    (not= (first a-seq) (first b-seq)) false
+    :else (recur (rest a-seq) (rest b-seq))))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [s a-seq
+         k 0]
+    (if (empty? s) nil
+        (if (pred (first s))
+          k
+          (recur (rest s) (inc k))))))
 
 (defn avg [a-seq]
   -1)
